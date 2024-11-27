8d9c56954fb96d2eb8ed28a2fa78f0aec3dd4a49
William Oldham <github@binaryoverload.co.uk>
2024-11-27 13:01:45 +0000

    Install biome

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,9 @@
 export function avg(...arr: (number | null | undefined)[]): number {
-  const filtered = arr.filter((v) => v != null) as number[];
-  return filtered.reduce((a, b) => a + b, 0) / filtered.length;
+  const filtered = arr.filter((v) => v != null) as number[]
+  return filtered.reduce((a, b) => a + b, 0) / filtered.length
 }
 
 export function round(value: number, precision = 0): number {
-  const multiplier = Math.pow(10, precision);
-  return Math.round(value * multiplier) / multiplier;
+  const multiplier = 10 ** precision
+  return Math.round(value * multiplier) / multiplier
 }
